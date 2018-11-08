49a0664261d1e4b9cbe4cac59702d2c9fabd895e
Junheng Wang <j765wang@uwaterloo.ca>
2018-11-08 12:09:14 -0500

    123

--- 1_vgg16_pretrain.py
+++ 1_vgg16_pretrain.py
@@ -1,118 +1,112 @@
-from network import VGG16
-from keras.preprocessing import image
-from keras.layers import GlobalAveragePooling2D, Dense, Dropout
-from keras.models import Model
-from keras import optimizers 
-from keras.callbacks import TensorBoard,ModelCheckpoint
-import argparse
-from time import time
+'''This script goes along the blog post
+"Building powerful image classification models using very little data"
+from blog.keras.io.
+It uses data that can be downloaded at:
+https://www.kaggle.com/c/dogs-vs-cats/data
+In our setup, we:
+- created a data/ folder
+- created train/ and validation/ subfolders inside data/
+- created cats/ and dogs/ subfolders inside train/ and validation/
+- put the cat pictures index 0-999 in data/train/cats
+- put the cat pictures index 1000-1400 in data/validation/cats
+- put the dogs pictures index 12500-13499 in data/train/dogs
+- put the dog pictures index 13500-13900 in data/validation/dogs
+So that we have 1000 training examples for each class, and 400 validation examples for each class.
+In summary, this is our directory structure:
+```
+data/
+    train/
+        dogs/
+            dog001.jpg
+            dog002.jpg
+            ...
+        cats/
+            cat001.jpg
+            cat002.jpg
+            ...
+    validation/
+        dogs/
+            dog001.jpg
+            dog002.jpg
+            ...
+        cats/
+            cat001.jpg
+            cat002.jpg
+            ...
+```
+'''
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dropout, Flatten, Dense
+from keras import applications
+
+# dimensions of our images.
+img_width, img_height = 150, 150
+
+top_model_weights_path = 'bottleneck_fc_model.h5'
+
+nb_train_samples = 80
+nb_validation_samples = 20
+epochs = 50
+batch_size = 16
 
 
-#################################################################################################################################
-#################################################################################################################################
-# Following steps are required to fine-tune the model
-#
-#  1. Specify the path to training and testing data, along with number of classes and image size.
-#  2. Do some random image transformations to increase the number of training samples and load the training and testing data
-#  3. Create VGG16 network graph(without top) and load imageNet pre-trained weights
-#  4. Add the top based on number of classes we have to the network created in step-3
-#  5. Specify the optimizer, loss etc and start the training
-##################################################################################################################################
-##################################################################################################################################
+def save_bottlebeck_features():
+    datagen = ImageDataGenerator(rescale=1. / 255)
 
-##### Step-1:
-############ Specify path to training and testing data. Minimum 100 images per class recommended.
-############ Default image size is 160    
-img_size=224
-ap = argparse.ArgumentParser()
-ap.add_argument("-train","--train_dir",type=str, required=True,help="(required) the train data directory")
-ap.add_argument("-val","--val_dir",type=str, required=True,help="(required) the validation data directory")
-ap.add_argument("-num_class","--class",type=int, default=2,help="(required) number of classes to be trained")
+    # build the VGG16 network
+    model = applications.VGG16(include_top=False, weights='imagenet')
 
-args = vars(ap.parse_args())
-
-##### Step-2:
-############ Do some random image transformations to increase the number of training samples
-############ Note that we are scaling the image to make all the values between 0 and 1. That's how our pretrained weights have been done too
-############ Default batch size is 8 but you can reduce/increase it depending on how powerful your machine is. 
-
-batch_size=10
-
-train_datagen = image.ImageDataGenerator(
-#        width_shift_range=0.1,
-#        height_shift_range=0.1,
-        rescale=1./255,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True)
-
-test_datagen = image.ImageDataGenerator(rescale=1. / 255)
-train_generator = train_datagen.flow_from_directory(
-        args["train_dir"],
-        target_size=(img_size, img_size),
+    generator = datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_width, img_height),
         batch_size=batch_size,
-        class_mode='categorical')
+        class_mode=None,
+        shuffle=False)
+    bottleneck_features_train = model.predict_generator(
+        generator, nb_train_samples // batch_size)
+    np.save(open('bottleneck_features_train.npy', 'w'),
+            bottleneck_features_train)
 
-validation_generator = test_datagen.flow_from_directory(
-        args["val_dir"],
-        target_size=(img_size,img_size),
+    generator = datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_width, img_height),
         batch_size=batch_size,
-        class_mode='categorical')
+        class_mode=None,
+        shuffle=False)
+    bottleneck_features_validation = model.predict_generator(
+        generator, nb_validation_samples // batch_size)
+    np.save(open('bottleneck_features_validation.npy', 'w'),
+            bottleneck_features_validation)
 
 
-##### Step-3:
-############ Create VGG-16 network graph without the last layers and load imagenet pretrained weights
-############ Default image size is 160    
-print('loading the model and the pre-trained weights...')
+def train_top_model():
+    train_data = np.load(open('bottleneck_features_train.npy'))
+    train_labels = np.array(
+        [0] * (nb_train_samples / 2) + [1] * (nb_train_samples / 2))
 
-base_model = VGG16.VGG16(include_top=False, weights='imagenet')
-## Here we will print the layers in the network
-i=0
-for layer in base_model.layers:
-    layer.trainable = False
-    i = i+1
-    print(i,layer.name)
-#sys.exit()
+    validation_data = np.load(open('bottleneck_features_validation.npy'))
+    validation_labels = np.array(
+        [0] * (nb_validation_samples / 2) + [1] * (nb_validation_samples / 2))
 
-##### Step-4:
-############ Add the top as per number of classes in our dataset
-############ Note that we are using Dropout layer with value of 0.2, i.e. we are discarding 20% weights
-############
+    model = Sequential()
+    model.add(Flatten(input_shape=train_data.shape[1:]))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1, activation='sigmoid'))
 
-x = base_model.output
-x = Dense(128)(x)
-x = GlobalAveragePooling2D()(x)
-x = Dropout(0.2)(x)
-predictions = Dense(args["class"], activation='softmax')(x)
+    model.compile(optimizer='rmsprop',
+                  loss='binary_crossentropy', metrics=['accuracy'])
 
+    model.fit(train_data, train_labels,
+              epochs=epochs,
+              batch_size=batch_size,
+              validation_data=(validation_data, validation_labels))
+    model.save_weights(top_model_weights_path)
 
-##### Step-5:
-############ Specify the complete model input and output, optimizer and loss
-
-tensorboard = TensorBoard(log_dir="logs/{}".format(time()))
-
-filepath = 'cv-tricks_pretrained_model.h5'
-checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1,save_best_only=True,save_weights_only=False, mode='min',period=1)
-callbacks_list = [checkpoint,tensorboard]
-
-
-model = Model(inputs=base_model.input, outputs=predictions)
-
-#model.compile(loss="categorical_crossentropy", optimizer=optimizers.SGD(lr=0.001, momentum=0.9),metrics=["accuracy"])
-model.compile(loss="categorical_crossentropy", optimizer=optimizers.Adam(),metrics=["accuracy"])
-#model.compile(loss="categorical_crossentropy", optimizer=optimizers.Adagrad(lr=0.01, epsilon=1e-08, decay=0.0),metrics=["accuracy"])
-
-num_training_img=1000
-num_validation_img=400
-stepsPerEpoch = num_training_img/batch_size
-validationSteps= num_validation_img/batch_size
-model.fit_generator(
-        train_generator,
-        steps_per_epoch=stepsPerEpoch,
-        epochs=20,
-        callbacks = callbacks_list,
-        validation_data = validation_generator,
-        validation_steps=validationSteps
-        )
-
-
+for i in range(5):
+    train_data_dir = 'data/train' + str(i+1)
+    validation_data_dir = 'data/validation' + str(i+1)
+    save_bottlebeck_features()
+    train_top_model()
